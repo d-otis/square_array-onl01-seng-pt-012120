2c8939aead2d6326aef6875928868c22ceb5806e
Dan Foley <dan@dan-foley.com>
2020-01-23 19:17:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,17 +2,18 @@ require "pry"
 
 array = [1,2,3]
 
-# def square_array(array)
-#   # your code here
-#   new_array = []
-#   array.each do |number|
-#     new_array << number ** 2
-#     # binding.pry
-#   end
-#   new_array
-# end
-
 def square_array(array)
-  # new_array = []
-  array.collect {|x| x ** 2}
+  # your code here
+  new_array = []
+  array.each do |number|
+    new_array << number ** 2
+    # binding.pry
+  end
+  new_array
 end
+
+
+# HIGHER LEVEL VERSION
+# def square_array(array)
+#   array.collect {|x| x ** 2}
+# end
